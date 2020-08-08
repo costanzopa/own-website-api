5827e312a85e4c7e63c9a36b9f6b8844df9438c1
costanzopa <costanzopa@gmail.com>
2020-08-08 17:30:25 -0300

    Adding signup route

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const { API_VERSION } = require('./config');
+const { API_VERSION } = require('./config.js');
 
 // Load routings
-// ...
+const userRoutes = require('./routers/user.js');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Router Basic
+app.use(`/api/${API_VERSION}`, userRoutes);
 
 module.exports = app;
---
+++ src/controllers/user.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/user.js');
+
+const signUp = (req, res) => {
+  const user = new User();
+
+  const { name, lastname, email, password, repeatPassword } = req.body;
+  user.name = name;
+  user.lastname = lastname;
+  user.email = email.toLowerCase();
+  user.role = 'admin';
+  user.active = false;
+  if (!password || !repeatPassword) {
+    res.status(404).send({ message: 'Las contraseñas son obligatorias.' });
+  } else {
+    if (password !== repeatPassword) {
+      res.status(404).send({ message: 'Las contraseñas no son iguales.' });
+    } else {
+      bcrypt.hash(password, 8, function (err, hash) {
+        if (err) {
+          res
+            .status(500)
+            .send({ message: 'Error al encriptar la contraseña.' });
+        } else {
+          user.password = hash;
+
+          user.save((err, userStored) => {
+            if (err) {
+              res.status(500).send({ message: 'El usuario ya existe.' });
+            } else {
+              if (!userStored) {
+                res.status(404).send({ message: 'Error al crear el usuario.' });
+              } else {
+                res.status(200).send({ user: userStored });
+              }
+            }
+          });
+        }
+      });
+    }
+  }
+};
+
+module.exports = {
+  signUp,
+};
