9d9c734de68a1cc27dd09206d4726f2a8182340c
costanzopa <costanzopa@gmail.com>
2020-08-22 20:31:01 -0300

    Adding run command

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-const { locale } = require('moment');
-const mongoose = require('mongoose');
-const app = require('./app');
+const { locale } = require("moment");
+const mongoose = require("mongoose");
+const app = require("./app");
 const {
   API_VERSION,
   IP_SERVER,
   PORT_SERVER,
   PORT_DB,
   HOST_DB,
-} = require('./config');
+} = require("./config");
 const port = process.env.PORT || PORT_SERVER;
 const host = process.env.HOST || IP_SERVER;
-const db_url =
-  process.env.DB_URL || `mongodb://${HOST_DB}:${PORT_DB}/own-website-api-db`;
+const db_url = process.env.DB_URL ||
+  `mongodb://${HOST_DB}:${PORT_DB}/own-website-api-db`;
 
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
 mongoose.connect(
   db_url,
@@ -23,14 +23,14 @@ mongoose.connect(
     if (err) {
       throw err;
     } else {
-      console.log('La conexion a la base de datos es correcta.');
+      console.log("La conexion a la base de datos es correcta.");
 
       app.listen(port, () => {
-        console.log('######################');
-        console.log('###### API REST ######');
-        console.log('######################');
+        console.log("######################");
+        console.log("###### API REST ######");
+        console.log("######################");
         console.log(`http://${host}:${port}/api/${API_VERSION}/`);
       });
     }
-  }
+  },
 );
